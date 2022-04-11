8ee406e1a51889fe4551ec586001d244136bdca6
Art3mis00Build <100994619+Art3mis00Build@users.noreply.github.com>
2022-04-11 17:18:49 +0200

    Add files via upload

---
+++ MVVM/View/StuffView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToDo_Prog.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for StuffView.xaml
+    /// </summary>
+    public partial class StuffView : UserControl
+    {
+        public StuffView()
+        {
+            InitializeComponent();
+            for (int i = 1; i < 32; i++)
+            {
+                TextBlock tmp = new TextBlock();
+                tmp.Text = Convert.ToString(i);
+                tmp.Margin = new System.Windows.Thickness(10,0,0,10);
+                tmp.Foreground = Brushes.White;
+                tmp.Background = new SolidColorBrush(Color.FromRgb(88, 68, 101));
+                tmp.FontSize = 64;
+                tmp.Width = 80;
+                tmp.Height = 80;
+                tmp.TextAlignment = TextAlignment.Center;
+                this.calendar.Children.Add(tmp);
+            }
+        }
+    }
+}
---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using ToDo_Prog.Core;
+
+namespace ToDo_Prog.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        //Kijelzőváltás észlelés és kezelés
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand StuffViewCommand { get; set; }
+        public RelayCommand HelpViewCommand { get; set; }
+
+        //Kinézet létrehozása
+        public HomeViewModel HomeVM { get; set; }
+        public StuffViewModel StuffVM { get; set; }
+        public HelpViewModel HelpVM { get; set; }
+
+        //Jelenlegi kinézet
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        //UI-ban a Content rész
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            StuffVM = new StuffViewModel();
+            HelpVM = new HelpViewModel();
+            CurrentView = HomeVM;
+
+            //Váltás észlelése
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            StuffViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = StuffVM;
+            });
+
+            HelpViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HelpVM;
+            });
+        }
+    }
+}
